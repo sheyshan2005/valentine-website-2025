5382e9121ce0cb0424d7edd65b2ffe43a3130bad
sheyshan2005 <sheyshan2005@gmail.com>
2026-02-06 17:24:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,6 @@ const CONFIG = {
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
         bears: ['🧸', '🐻']                       // Cute bear emojis
-        elephant: ['🐘',]                       // Cute elephant emojis
     },
 
     // Questions and answers
